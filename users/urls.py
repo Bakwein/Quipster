3facea58a96d75bf9cc2b58a872fa7955086c6e4
Bakwein <sefatuncaa3@gmail.com>
2024-04-29 19:21:38 +0300

    initial commit

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
+    path('profile/', profile, name='profile'),
+]
