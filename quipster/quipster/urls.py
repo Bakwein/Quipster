f06768ad0845b43070d13c1e69fe46c316bd402a
Alsond5 <ahmet_idris_0109@hotmail.com>
2024-04-30 01:41:15 +0300

    Add navbar

--- quipster/quipster/urls.py
+++ quipster/quipster/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/users', include("users.urls")),
+    path('users/', include("users.urls")),
 ]
 
 
--- quipster/users/views.py
+++ quipster/users/views.py
@@ -3,12 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def register(request):
-    pass
-    #return render(request, 'users/register.html')
+    return render(request, 'users/register.html')
 
 def login(request):
-    pass
-    #return render(request, 'users/login.html')
+    return render(request, 'users/login.html')
 
 def logout(request):
     pass
