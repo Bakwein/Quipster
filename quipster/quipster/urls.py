f27352c0495c8f5dc309f51e41363cf28a326aeb
Alsond5 <ahmet_idris_0109@hotmail.com>
2024-05-03 19:03:33 +0300

    Add posts app

--- quipster/quipster/urls.py
+++ quipster/quipster/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include("users.urls")),
+    path('', include("posts.urls")),
 ]
 
 
