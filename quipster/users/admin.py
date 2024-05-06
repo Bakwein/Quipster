6cbfbc4f85c7b7bae444089ee33208bcd2b74ffb
Bakwein <sefatuncaa3@gmail.com>
2024-05-06 20:35:26 +0300

    fetch: following,follower attributes are created with Alsond5

--- quipster/users/admin.py
+++ quipster/users/admin.py
@@ -3,7 +3,11 @@ from .models import TwitterUser
 # Register your models here.
 
 class TwitterUserAdmin(admin.ModelAdmin):
-    list_display = ('user',)
+    list_display = ('user', 'get_followings', 'display_followers')
+
+    def display_followers(self, obj):
+        return ", ".join(obj.get_followers())
+
     #list_filter = ("status",)
     #search_fields = ['title', 'content']
     #prepopulated_fields = {'slug': ('title',)}
--- quipster/users/models.py
+++ quipster/users/models.py
@@ -5,8 +5,14 @@ from django.contrib.auth.models import User
 
 class TwitterUser(models.Model):
     user= models.ForeignKey(User, on_delete=models.CASCADE)
+    following = models.ManyToManyField(User, related_name='following')
+    
+    def get_followings(self):
+        return [user.username for user in self.following.all()]
 
+    def get_followers(self):
+        return [user.user.username for user in TwitterUser.objects.filter(following=self.user)]
 
     def __str__(self):
-        return self.user.first_name
+        return self.user.username
     
