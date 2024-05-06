7e0518b6318c1f213d6553b261affecfe8109c38
Bakwein <sefatuncaa3@gmail.com>
2024-05-06 19:55:30 +0300

    tweet model creation

--- quipster/posts/admin.py
+++ quipster/posts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Tweet
 
 # Register your models here.
+
+class TweetAdmin(admin.ModelAdmin):
+    list_display = ('user', 'get_likes', 'get_comments_count', 'is_comment', 'created_at')
+    list_filter = ("user", "is_comment", "created_at", "updated_at", "parent")
+    search_fields = ['content']
+    #prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Tweet, TweetAdmin)
--- quipster/posts/models.py
+++ quipster/posts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from users.models import TwitterUser
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Tweet(models.Model):
+    content = models.CharField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='likes')
+    is_comment = models.BooleanField(default=False)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.content
+    
+    def get_comments(self):
+        return Tweet.objects.filter(parent=self)
+    
+    def get_comments_count(self):
+        return Tweet.objects.filter(parent=self).count()
+    
+    def get_likes(self):
+        return self.likes.count()
