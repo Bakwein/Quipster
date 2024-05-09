13ab359cc6d1b6dcb0fef2c190ac5054bd4fea38
Bakwein <sefatuncaa3@gmail.com>
2024-05-10 02:31:15 +0300

    lang dosyaları eklendi

---
+++ quipster/posts/langs.py
@@ -0,0 +1,13 @@
+context = {
+    'en': {
+        'page_title' : 'Feeds',
+        'popular' : 'Popular',
+        'Latest' : 'Latest',
+
+    },
+    'tr':{
+        'page_title' : 'Akışlar',
+        'popular' : 'Popüler',
+        'Latest' : 'En Yeni',
+    }
+}
---
+++ quipster/users/langs.py
@@ -0,0 +1,116 @@
+context = {
+    'en': {
+        #login page
+        'warning_not_title' : 'Warning',
+        'sign_to': 'Sign in to Quipster',
+        'acc_check' : 'Don\'t have an account yet?',
+        'sign_up_here' : 'Sign up here',
+        'sign_google': 'Sign in with Google',
+        'username' : 'Username',
+        'password' : 'Password',
+        'forgot_pass' : 'Forgot Password',
+        'sign_in' : 'Sign in',
+
+        #profile
+        'edit_profile': 'Edit Profile',
+        'save': 'Save',
+        #'username': 'Username',
+        'name': 'Name',
+        'Description': 'Description',
+        'post': 'Post',
+        #'edit_profile': 'Edit Profile',
+        'join_date': 'Joined', #burası gario oldu
+        'followers':  'Followers',
+        'following': 'Following',
+        'posts': 'Posts',
+        'replies': 'Replies',
+        'like': 'Like',
+
+        #recover
+        'forgot_q' : 'Forgot password?',
+        'remember_q': 'Remembered your password?',
+        #'sign_in' : 'Sign in',
+        'email_adress': 'Email Adress',
+        'reset_pass': 'Reset Password',
+
+        #register
+        'sign_up_q': 'Sign Up to Quipster',
+        'already_have': 'Already have an account?',
+        'sign_in_here': 'Sign in here',
+        'sign_up_wGoogle': 'Sign up with Google',
+        #'username' : 'Username',
+        'first_name': 'First Name',
+        'surname': 'Surname',
+        #'password' : 'Password',
+        'confirm_pass': 'Confirm Password',
+        'term1' : 'I accept the',
+        'term2' : 'Terms and Conditions',
+        'sign_up': 'Sign Up',
+
+
+
+
+    },
+    'tr':{
+        #login page
+        'warning_not_title' : 'Uyarı',
+        'sign_to': 'Quipster\'a giriş yap',
+        'acc_check' : 'Henüz bir hesabınız yok mu?',
+        'sign_up_here' : 'Buradan kaydolun',
+        'sign_google': 'Google ile giriş yap',
+        'username' : 'Kullanıcı Adı',
+        'password' : 'Şifre',
+        'forgot_pass' : 'Şifremi Unuttum',
+        'sign_in' : 'Giriş Yap',
+
+        #profile
+        'edit_profile': 'Profili Düzenle',
+        'save': 'Kaydet',
+        #'username': 'Kullanıcı Adı',
+        'name': 'İsim',
+        'Description': 'Açıklama',
+        'post': 'Gönderi',
+        #'edit_profile': 'Profili Düzenle',
+        'join_date': 'Tarihinde katıldı',
+        'followers':  'Takipçiler',
+        'following': 'Takip edilenler',
+        'posts': 'Gönderiler',
+        'replies': 'Yanıtlar',
+        'like': 'Beğeni',
+
+        #recover
+        'forgot_q' : 'Şifrenizi mi unuttunuz?',
+        'remember_q': 'Şifrenizi hatırladınız mı?',
+        #'sign_in' : 'Giriş Yap',
+        'email_adress': 'Email Adresi',
+        'reset_pass': 'Şifreyi Sıfırla',
+
+
+        #register
+        'sign_up_q': 'Quipster\'a Kaydolun',
+        'already_have': 'Zaten bir hesabınız mı var?',
+        'sign_in_here': 'Buradan giriş yapın',
+        'sign_up_wGoogle': 'Google ile kaydolun',
+        #'username' : 'Kullanıcı Adı',
+        'first_name': 'Ad',
+        'surname': 'Soyad',
+        #'password' : 'Şifre',
+        'confirm_pass': 'Şifreyi Onayla',
+        'term1': 'Kabul', # sıra farklı olduğundan böyle oldu :D
+        'term2': 'Kullanım Koşulları',
+        'sign_up': 'Kaydol',
+
+        
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
